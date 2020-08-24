fe6b2785f77e9bb2eaeddc65ede80bc2e6d53ab0
Marco <marco.sautto@gmail.com>
2020-08-24 19:01:35 +0200

    added prompt on userList click

--- src/sample/groupPage/GroupUserController.java
+++ src/sample/groupPage/GroupUserController.java
@@ -1,17 +1,25 @@
 package sample.groupPage;
 
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonBar;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.ListView;
 import sample.PostListViewController;
 import sample.UserListViewController;
 import sample.api.Mockdatabase;
+import sample.model.Course;
 import sample.model.GroupMember;
 import sample.model.Post;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 public class GroupUserController implements Initializable {
@@ -45,6 +53,38 @@ public class GroupUserController implements Initializable {
     public void initialize(URL location, ResourceBundle resources) {
         //postListView.setItems(postObservableList);
         //postListView.setCellFactory(postListView -> new PostListViewController());
+
+        userListView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener<GroupMember>() {
+
+            @Override
+            public void changed(ObservableValue<? extends GroupMember> observable, GroupMember oldValue, GroupMember newValue) {
+                // Your action here
+                System.out.println("Selected item: " + newValue.getUserId());
+
+                //DashboardController dashboardController = new DashboardController();
+
+                Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+                alert.setTitle("Utente selezionato");
+                alert.setHeaderText("Cosa vuoi fare con l'utente " + newValue.getUserId() + "?");
+                alert.setContentText("Seleziona un'opzione.");
+
+                ButtonType buttonAdmin= new ButtonType("Rendi amministratore");
+                ButtonType buttonKick = new ButtonType("Caccia dal gruppo");
+                ButtonType buttonTypeCancel = new ButtonType("Cancel", ButtonBar.ButtonData.CANCEL_CLOSE);
+
+                alert.getButtonTypes().setAll(buttonKick, buttonAdmin, buttonTypeCancel);
+
+                Optional<ButtonType> result = alert.showAndWait();
+                if (result.get() == buttonAdmin){
+                    //TODO: API make user as admin
+                } else if (result.get() == buttonKick) {
+                    //TODO: kick user from group
+                }  else {
+                    // ... user chose CANCEL or closed the dialog
+                }
+            }
+        });
+
 
     }
 
