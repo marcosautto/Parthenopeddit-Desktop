4821183531f97ead112bee84e8074141dac23812
Marco <marco.sautto@gmail.com>
2020-08-24 19:39:54 +0200

    added elements in profileLayout

--- src/sample/ProfileController.java
+++ src/sample/ProfileController.java
@@ -1,10 +1,25 @@
 package sample;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class ProfileController {
 
     @FXML
-    Label usernameLabel;
+    private Label usernameLabel;
+
+    @FXML
+    private Label usernameShownLabel;
+
+    @FXML
+    private Button changeUsernameButton;
+
+    @FXML
+    private Button showUserPostButton;
+
+    @FXML
+    private Button showUserReviewButton;
+
+
 }
