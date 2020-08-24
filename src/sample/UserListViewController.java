a91d65e12f0df2bed3257cf9e01f9a129a48a8ca
Marco <marco.sautto@gmail.com>
2020-08-24 15:52:02 +0200

    added userlistview controller

---
+++ src/sample/UserListViewController.java
@@ -0,0 +1,58 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.AnchorPane;
+import sample.model.GroupMember;
+import sample.model.Post;
+
+import java.io.IOException;
+
+public class UserListViewController extends ListCell<GroupMember> {
+
+    @FXML
+    private Label usernameLabel;
+
+    @FXML
+    private Label joinedOnLabel;
+
+    @FXML
+    private AnchorPane anchorPane;
+
+    private FXMLLoader mLLoader;
+
+    @Override
+    protected void updateItem(GroupMember user, boolean empty) {
+        super.updateItem(user, empty);
+
+        if(empty || user == null) {
+
+            setText(null);
+            setGraphic(null);
+
+        } else {
+            if (mLLoader == null) {
+                mLLoader = new FXMLLoader(getClass().getResource("fxml/ListCell/GroupMemberListCell.fxml"));
+                mLLoader.setController(this);
+
+                try {
+                    mLLoader.load();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+            usernameLabel.setText(user.getUserId());
+            joinedOnLabel.setText(user.getJoinDate());
+
+            setText(null);
+            setGraphic(anchorPane);
+        }
+
+    }
+}
--- src/sample/groupPage/GroupPostController.java
+++ src/sample/groupPage/GroupPostController.java
@@ -1,4 +1,52 @@
 package sample.groupPage;
 
-public class GroupPostController {
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+import sample.PostListViewController;
+import sample.api.Mockdatabase;
+import sample.model.Post;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class GroupPostController implements Initializable {
+
+    @FXML
+    private ListView<Post> postListView;
+
+    private ObservableList<Post> postObservableList;
+
+    private static GroupPostController instance;
+    // static method to get instance of view
+    public static GroupPostController getInstance() {
+        return instance;
+    }
+
+    public GroupPostController()  {
+
+        instance = this;
+        postObservableList = FXCollections.observableArrayList();
+        Mockdatabase mockdatabase = new Mockdatabase();
+
+        //add some Students
+        postObservableList.addAll(
+        );
+
+
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        //postListView.setItems(postObservableList);
+        //postListView.setCellFactory(postListView -> new PostListViewController());
+
+    }
+
+    public void sendPosts(ObservableList<Post> posts){
+        postListView.setItems(posts);
+        postListView.setCellFactory(postListView -> new PostListViewController());
+    }
 }
